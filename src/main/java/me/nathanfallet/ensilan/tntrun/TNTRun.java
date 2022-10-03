0bfcf1e9d66c0a5ac6d9117714e0a0baecd38b85
Nathan Fallet <contact@nathanfallet.me>
2022-10-03 21:19:19 +0200

    Fix scoreboard

--- src/main/java/me/nathanfallet/ensilan/tntrun/TNTRun.java
+++ src/main/java/me/nathanfallet/ensilan/tntrun/TNTRun.java
@@ -225,7 +225,7 @@ public class TNTRun extends JavaPlugin {
 				for (Game g : getGames()) {
 					if (g.getGameNumber() == zp.getCurrentGame()) {
 						lines.add("§c");
-						lines.add("§c§lJoueurs");
+						lines.add("§c§lJoueurs :");
 						lines.add("§f" + g.getPlayers().size() + "/" + g.getMaxPlayers());
 						lines.add("§d");
 						lines.add("§d§lStatut :");
